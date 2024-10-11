4f373d93490b5eb73a5749d5b12065480c909058
zippy-zip <122580849+zippy-zip@users.noreply.github.com>
2024-10-11 16:23:26 -0700

    Plotting-Data.py
    New updated version from FROST

--- Plotting-Data.py
+++ Plotting-Data.py
@@ -17,7 +17,7 @@ import re
 
 ''' input file path '''
 
-input_file_path = 
+input_file_path = r
 
 # Initialize lists and variables
 calibrated_ws = []
@@ -45,7 +45,7 @@ with open(input_file_path, 'r') as file:
         
         if "calibration factor" in line:
             cf = int(re.search(int_pattern, line).group())
-            print("Calibration Factor:", cf)
+            print(cf)
             
         elif line.startswith('| step size:'):
             step_size = int(re.search(int_pattern, line).group())
@@ -65,8 +65,8 @@ with open(input_file_path, 'r') as file:
                 disp += step_size
             
             pulse_disps.append(disp)
-            current_loop['x'].append(disp)
-            current_loop['y'].append(float(ws[0]))
+            current_loop['x'].append(disp/40)
+            current_loop['y'].append(float(ws[0])*0.00981)
 
             # Check if the direction changed from down to up, indicating the start of a new loop
             if previous_direction == "up" and direc == "down" and current_loop['x']:
@@ -89,22 +89,53 @@ colors = plt.cm.viridis(np.linspace(0, 1, len(loops)))
 for i, loop in enumerate(loops):
     plt.plot(loop['x'], loop['y'], 'o-', label=f'Loop {i+1}', linewidth=1, markersize=2, color=colors[i])
 
-# Add an arrow for "Up"
-plt.annotate('Up', xy=(0.75, 0.3), xycoords='axes fraction',
-             xytext=(0.9, 0.5), textcoords='axes fraction',
-             arrowprops=dict(facecolor='green', shrink=0.05, width=1.5),
-             horizontalalignment='right')
 
-# Add an arrow for "Down"
-plt.annotate('Down', xy=(0.25, 0.45), xycoords='axes fraction',
-             xytext=(0.1, 0.3), textcoords='axes fraction',
-             arrowprops=dict(facecolor='red', shrink=0.05, width=1.5),
-             horizontalalignment='left')
+label = input("Spring or Fabric?: ")
+run = input("Run: ")
+delay = input("Delay: ")
 
-# Labeling axes and title
-plt.xlabel('Pulse Displacement')
-plt.ylabel('TT Weight (g)')
-plt.title('Pulse Displacement vs. TT Weight')
+text_input = "Run: "+run+", Delay: "+delay
+
+plt.xlabel('Displacement (cm)', fontsize=20)
+plt.ylabel('Applied Force (N)', fontsize=20)
+plt.title('Applied Force vs. Displacement with '+label, fontsize=20, fontweight='bold')
 plt.legend()
+
+plt.text(0.95, 0.05, text_input, transform=plt.gca().transAxes,
+         fontsize=10, verticalalignment='bottom', horizontalalignment='right',
+         bbox=dict(facecolor='white', alpha=0.5))
+
 plt.show()
 
+
+
+# plt.annotate('Stretching', xy=(0.7, 0.4), xycoords='axes fraction',
+#               xytext=(0.4, 0.1), textcoords='axes fraction',
+#               arrowprops=dict(facecolor='green', shrink=0.05, width=1.5),
+#               horizontalalignment='right')
+
+# # Add an arrow for "Down"
+# plt.annotate('Relaxing', xy=(0.3, 0.5), xycoords='axes fraction',
+#               xytext=(0.7, 0.9), textcoords='axes fraction',
+#               arrowprops=dict(facecolor='red', shrink=0.05, width=1.5),
+#               horizontalalignment='left')
+
+
+# #Add an arrow for "Up"
+# plt.annotate('Stretching', xy=(0.6, 0.8), xycoords='axes fraction',
+#               xytext=(0.4, 0.6), textcoords='axes fraction',
+#               arrowprops=dict(facecolor='green', shrink=0.05, width=1.5),
+#               horizontalalignment='right')
+
+# # Add an arrow for "Down"
+# plt.annotate('Relaxing', xy=(0.3, 0.05), xycoords='axes fraction',
+#               xytext=(0.8, 0.4), textcoords='axes fraction',
+#               arrowprops=dict(facecolor='red', shrink=0.05, width=1.5),
+#               horizontalalignment='left')
+
+
+# Labeling axes and title
+
+# manually setting x and y axes
+# plt.xlim(8.5,10.25) 
+# plt.ylim(2.2, 3.3)
